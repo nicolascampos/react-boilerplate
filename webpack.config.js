9c9183c86b3fd4821ceeddaabefe422bd9830a07
nicolascampos <nicolas.campos@monotype.com>
2020-02-03 11:28:20 -0300

    chore: setup webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const BUILD_DIR = path.join(__dirname, 'dist');
+const APP_DIR = path.join(__dirname, 'src');
+
+module.exports = {
+  entry: {
+    bundle: APP_DIR + '/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: BUILD_DIR,
+    compress: true,
+    hot: true,
+    port: 9001
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: APP_DIR + '/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
